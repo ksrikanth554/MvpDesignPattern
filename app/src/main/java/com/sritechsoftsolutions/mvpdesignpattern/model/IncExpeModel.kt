e2cf1fa9c18f634b7d18992163a4a2a92981707f
ksrikanth554 <ksrikanth554@mgial.com>
2019-02-23 17:07:56 +0530

    updated

--- app/src/main/java/com/sritechsoftsolutions/mvpdesignpattern/model/IncExpeModel.kt
+++ app/src/main/java/com/sritechsoftsolutions/mvpdesignpattern/model/IncExpeModel.kt
@@ -1,14 +1,50 @@
 package com.sritechsoftsolutions.mvpdesignpattern.model
 
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.widget.SimpleCursorAdapter
 import com.sritechsoftsolutions.mvpdesignpattern.bean.IncExpBean
 import com.sritechsoftsolutions.mvpdesignpattern.presenter.IncExpPresenterApi
+import com.sritechsoftsolutions.mvpdesignpattern.view.IncExpeViewApi
+import com.sritechsoftsolutions.mvpdesignpattern.view.MainActivity
+import java.util.*
 
-class IncExpeModel:IncExpPresenterApi {
+class IncExpeModel(var api:IncExpeViewApi):IncExpPresenterApi {
+
+    var dBase:SQLiteDatabase?=null
+    var mActivity:MainActivity?=null
+    init {
+        mActivity=api as MainActivity
+        dBase=mActivity!!.openOrCreateDatabase("money",Context.MODE_PRIVATE,null)
+        dBase?.execSQL("create table if not exists usermoney(_id integer primary key autoincrement,date varchar(20),money integer(20),description varchar(50),type varchar(20) ) ")
+
+
+    }
+
     override fun save(bean: IncExpBean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var cv=ContentValues()
+        cv.put("date",bean.date)
+        cv.put("money",bean.money)
+        cv.put("description",bean.desc)
+        cv.put("type",bean.type)
+
+        var status=dBase?.insert("usermoney",null,cv)
+        if (status!=-1L)
+        {
+            api.saveResponse("row inserted successfully")
+
+        }
+        else
+        {
+            api.saveResponse("row insertion failed")
+
+        }
     }
 
     override fun read() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var c:Cursor?=dBase?.query("usermoney",null,null,null,null,null,null)
+        api.readResponse(c!!)
     }
 }
--- app/src/main/java/com/sritechsoftsolutions/mvpdesignpattern/view/MainActivity.kt
+++ app/src/main/java/com/sritechsoftsolutions/mvpdesignpattern/view/MainActivity.kt
@@ -3,19 +3,65 @@ package com.sritechsoftsolutions.mvpdesignpattern.view
 import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SimpleCursorAdapter
+import android.widget.Toast
 import com.sritechsoftsolutions.mvpdesignpattern.R
+import com.sritechsoftsolutions.mvpdesignpattern.bean.IncExpBean
+import com.sritechsoftsolutions.mvpdesignpattern.model.IncExpeModel
+import com.sritechsoftsolutions.mvpdesignpattern.presenter.IncExpPresenterApi
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),IncExpeViewApi {
     override fun saveResponse(msg: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        Toast.makeText(this@MainActivity,msg,Toast.LENGTH_LONG).show()
+
     }
 
     override fun readResponse(c: Cursor) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var cAdapter=SimpleCursorAdapter(this@MainActivity,R.layout.displayview,c, arrayOf("_id","date","money","description","type"),
+            intArrayOf(R.id.txtid,R.id.txtdate,R.id.txtmoney,R.id.txtdescrption,R.id.txttype),0)
+        lview.adapter=cAdapter
+        var isum=0
+        var esum=0
+        while (c.moveToNext())
+        {
+            if (c.getString(4).equals("income",true))
+            {
+                isum=isum+c.getInt(2)
+            }
+            else
+            {
+                esum=esum+c.getInt(2)
+            }
+            txtincome.setText("Income: $isum")
+            txtexpense.setText("Expnese: $esum")
+        }
+
+
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var model:IncExpPresenterApi=IncExpeModel(this@MainActivity)
+
+        btnsave.setOnClickListener {
+            var bean=IncExpBean(edtdate.text.toString(),
+                edtmoney.text.toString().toInt(),
+                edtdesc.text.toString(),spntype.selectedItem.toString())
+            model.save(bean)
+            edtdate.setText("")
+            edtmoney.setText("")
+            edtdesc.setText("")
+            spntype.setSelection(0)
+
+        }
+        btnread.setOnClickListener {
+            model.read()
+
+        }
     }
 }
